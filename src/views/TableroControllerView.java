7ae79af91eae6df52c8218571667a325ea92cce1
Gabriel <hernandezgabriel671@gmail.com>
2023-12-07 17:05:02 -0400

    prueba desde netbeans

--- src/views/TableroControllerView.java
+++ src/views/TableroControllerView.java
@@ -1,48 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package views;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DataFormat;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
-import javafx.scene.layout.GridPane;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.layout.*;
+import javafx.scene.shape.*;
 
-/**
- *
- * @author net
- */
 public class TableroControllerView implements Initializable {
-    
-    @FXML
-    private Label NombreJugador;
-    //private TableroBatalla tableroModel; // Agregamos una referencia al modelo
 
-   /* public void setTableroModel(TableroBatalla tableroModel) {
-        this.tableroModel = tableroModel;
-    }*/
+    @FXML
+    private GridPane gridJugador;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
-        
+        double gridAncho = gridJugador.getPrefWidth();
+        double gridAlto = gridJugador.getPrefHeight();
+
+        int columnasTablero = 10;
+        int filasTablero = 10;
+
+        for (int x = 0; x < columnasTablero; x++) {
+            for (int y = 0; y < filasTablero; y++) {
+                Rectangle rectangulo = new Rectangle();
+                rectangulo.setWidth(gridAncho / columnasTablero - 2);
+                rectangulo.setHeight(gridAlto / filasTablero - 2);
+
+                rectangulo.applyCss();
+
+                gridJugador.add(rectangulo, x, y);
+                gridJugador.setAlignment(Pos.CENTER);
+
+            }
+        }
     }
-    
+
     @FXML
     public void onAbandonarButtonClick() {
-         Stage stage = (Stage) NombreJugador.getScene().getWindow();
-        stage.close();
+
     }
-   
+
 }
