00e6d0d6f9bb99785907686cc555fb151220aa10
net <net@NetKBs>
2023-12-07 13:41:48 -0400

    Incicio inerfaz carga

--- src/views/InicioControllerView.java
+++ src/views/InicioControllerView.java
@@ -1,14 +1,55 @@
 package views;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-/**
- *
- * @author net
- */
-public class InicioControllerView {
+
+public class InicioControllerView implements Initializable{
     
+    
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private Button BotonJugar;
+    @FXML
+    private TextField Usuario;
+    @FXML
+    
+   
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+       
+    }
+    @FXML
+    protected void onExitButtonClick() {
+        Stage stage = (Stage) welcomeText.getScene().getWindow();
+        stage.close();
+    }
+    @FXML
+    public void onJugarButtonClick() throws IOException {
+        try {
+            // Cargar el nuevo FXML (tablero.fxml)
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("tablero.fxml"));
+            AnchorPane tableroPane = fxmlLoader.load();
+            // Crear una nueva escena
+            Scene tableroScene = new Scene(tableroPane, 765, 443);
+            // Obtener el escenario actual (ventana)
+            Stage stage2 = (Stage) BotonJugar.getScene().getWindow();
+            // Configurar la nueva escena en el escenario actual
+            stage2.setScene(tableroScene);
+            stage2.setTitle("Batalla Naval - Tablero");
+        } finally {
+        }
+    }
 }
